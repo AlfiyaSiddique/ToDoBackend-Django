27307f1c70c84bde998720111a1f4d203559d3a9
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-12-03 23:08:49 +0530

    Deployment error Fix

--- todobackend/todobackend/settings.py
+++ todobackend/todobackend/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -14,7 +13,6 @@ SECRET_KEY = 'django-insecure-@cpujk_o4=h8x87^e13!5y80@wrfnvif61#+3-!e5f1(zn^$1r
 DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
-
 
 # Application definition
 
@@ -61,7 +59,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todobackend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -71,7 +68,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -91,7 +87,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -102,7 +97,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -124,6 +118,7 @@ REST_FRAMEWORK = {
 }
 
 import os
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
