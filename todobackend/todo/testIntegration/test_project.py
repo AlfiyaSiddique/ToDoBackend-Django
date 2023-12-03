97dfb72f052c615953ee8d2a92f7e797556d3ed5
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-12-04 04:33:50 +0530

    Final Commit

---
+++ todobackend/todo/testIntegration/test_project.py
@@ -0,0 +1,34 @@
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
+class AdminCSSTestCase(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome()  # Use the appropriate webdriver
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_site_load_or_crash(self):
+        urls_to_check = [self.live_server_url, f"{self.live_server_url}/admin/",
+                         f"{self.live_server_url}/tag/"]
+
+
+        for url in urls_to_check:
+            self.selenium.get(url)
+            try:
+                body_element = self.selenium.find_element(By.TAG_NAME, "body")
+            except NoSuchElementException:
+                body_element = None
+
+            self.assertIsNotNone(body_element, f"Site crashed or failed to load at {url}")
+
+            time.sleep(30)
