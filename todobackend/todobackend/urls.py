4e7172ec0698ee9ec2e3edf36b7b221d11887b48
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-12-18 16:04:08 +0530

    Short Fix

--- todobackend/todobackend/urls.py
+++ todobackend/todobackend/urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from todo import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('todobackend.urls'))
+    path('todo/<int:pk>/', views.UpdateTodo.as_view(), name="updateTodo"),
+    path('/todo', views.TodoViews.as_view(), name="allTodo"),
+    path('create', views.CreateTodo.as_view(), name="createTodo"),
+    path('delete/<int:pk>', views.DeleteTodo.as_view(), name="deleteTodo"),
+    path('create/tag', views.CreateTag.as_view(), name="createTag"),
+    path('tag', views.TagView.as_view(), name="allTag"),
+    path('tag/<int:pk>/', views.UpdateTag.as_view(), name="updateTag"),
+    path('delete/tag/<int:pk>', views.DeleteTag.as_view(), name="deleteTag"),
 ]
--- todobackend/urls.py
+++ todobackend/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path
-from todo import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('todo/<int:pk>/', views.UpdateTodo.as_view(), name="updateTodo"),
-    path('', views.TodoViews.as_view(), name="allTodo"),
-    path('create', views.CreateTodo.as_view(), name="createTodo"),
-    path('delete/<int:pk>', views.DeleteTodo.as_view(), name="deleteTodo"),
-    path('create/tag', views.CreateTag.as_view(), name="createTag"),
-    path('tag', views.TagView.as_view(), name="allTag"),
-    path('tag/<int:pk>/', views.UpdateTag.as_view(), name="updateTag"),
-    path('delete/tag/<int:pk>', views.DeleteTag.as_view(), name="deleteTag"),
+    path('admin/', admin.site.urls),
+    path('api/', include('todobackend.urls')),
 ]
