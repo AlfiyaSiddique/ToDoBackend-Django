0aa715bb87749fefbddcc2af1d4d359a1ac9a66e
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-12-20 22:22:40 +0530

    Small route fix

--- todobackend/todobackend/urls.py
+++ todobackend/todobackend/urls.py
@@ -3,7 +3,7 @@ from todo import views
 
 urlpatterns = [
     path('todo/<int:pk>/', views.UpdateTodo.as_view(), name="updateTodo"),
-    path('/todo', views.TodoViews.as_view(), name="allTodo"),
+    path('todo', views.TodoViews.as_view(), name="allTodo"),
     path('create', views.CreateTodo.as_view(), name="createTodo"),
     path('delete/<int:pk>', views.DeleteTodo.as_view(), name="deleteTodo"),
     path('create/tag', views.CreateTag.as_view(), name="createTag"),
